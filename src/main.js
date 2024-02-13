49de32648865d3487aa1be987f7eb532693ad2f7
Murilo Ferreira dos Santos <murilo@jogoglobal.com>
2024-02-13 01:05:36 -0300

    fixfix

--- src/main.js
+++ src/main.js
@@ -31,15 +31,14 @@
 // src/main.js
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+
 import vuetify from "./plugins/vuetify";
-import firebaseConfig from "./services/firebaseConfig"; // Importe o arquivo de configuração do Firebase
+// Importe o arquivo de configuração do Firebase
 
 // O Firebase é inicializado no próprio firebaseConfig, portanto, não é necessário chamá-lo aqui novamente.
 
 const app = createApp(App);
-app.use(firebaseConfig);
-app.use(router);
+
 app.use(vuetify);
 
 app.mount("#app");

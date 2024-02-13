4ec71e7a2e7e118f8614b7f9f36326f18c6bca5d
Murilo Ferreira dos Santos <murilo@jogoglobal.com>
2024-02-13 00:42:43 -0300

    test fix

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,6 @@
-// Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
-// Vuetify
-import { createVuetify } from 'vuetify'
-
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+const vuetify = createVuetify({
+  ssr: true,
+})
